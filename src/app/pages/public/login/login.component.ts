91668c670894953d572b3a532e4966b787ccc149
German Martinez <germancmartinez@hotmail.com>
2020-10-26 22:35:54 -0500

    adding select to choose authentication way

--- src/app/pages/public/login/login.component.ts
+++ src/app/pages/public/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PrefixI } from 'src/app/shared/interfaces/PrefixI';
 import { UserI } from 'src/app/shared/interfaces/UserI';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { PrefijoService } from 'src/app/shared/services/prefijo.service';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +14,9 @@ export class LoginComponent implements OnInit {
 
   title: string = "Hola Mundo";
   color: string = "red"
+  isLoginMail: boolean = true;
+
+  listaPrefijos: PrefixI[];
 
   user: UserI = {
     correo:'',
@@ -26,10 +31,21 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private router:Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private prefijoService: PrefijoService,
     ) { }
 
   ngOnInit(): void {
+    this.getPrefix();
+  }
+
+  getPrefix(){
+    this.prefijoService.getAll().subscribe(res => {
+      this.listaPrefijos = res;
+      console.log(this.listaPrefijos);
+    }, err => {
+      console.log(err);
+    });
   }
 
   goToRegister() {
@@ -37,12 +53,26 @@ export class LoginComponent implements OnInit {
   }
 
   doLogin() {
-    this.authService.loginMail(this.user).subscribe(res => {
-      console.log(res);
-      this.router.navigate(['home']);
-    }, err => {
-      console.log(err);
-    });
-    console.log(this.user);
+    if(this.isLoginMail){
+      this.authService.loginMail(this.user).subscribe(res => {
+        console.log(res);
+        this.router.navigate(['home']);
+      }, err => {
+        console.log(err);
+      });
+      console.log(this.user);
+    } else {
+      this.authService.loginPhone(this.user).subscribe(res => {
+        console.log(res);
+        this.router.navigate(['home']);
+      }, err => {
+        console.log(err);
+      });
+      console.log(this.user);
+    }
+  }
+
+  setLoginMail(evt: any) {
+    this.isLoginMail = evt.toElement.id === 'mail';
   }
 }
